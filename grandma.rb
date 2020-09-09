0accd40acd967144cfae950ff83d9798ab7b3554
Tabatha Roman <romantabatha17@gmail.com>
2020-09-09 16:07:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,9 +16,9 @@ def speak_to_grandma(speak)
   if speak == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
   elsif speak == speak.upcase
-    'NO, NOT SINCE 1938'
+    'NO, NOT SINCE 1938!'
   else 
-    'HUH?! SPEAK UP, SONNY'
+    'HUH?! SPEAK UP, SONNY!'
   end
 end
 
